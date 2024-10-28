9a52313671b7dd87d549c9faf7bb7300be5e5726
gle.bushkaa <gleb.mokryy@gmail.com>
2024-10-28 17:03:36 +0200

    finished

--- src/main/java/jom/com/softserve/s6/task2/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task2/MyUtils.java
@@ -1,12 +1,14 @@
 package jom.com.softserve.s6.task2;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
 
 public class MyUtils {
     public static String getDateAfterToday(int years, int months, int days) {
-        //Write code there
-        return "";
+        return LocalDate.now()
+                .plusYears(years)
+                .plusMonths(months)
+                .plusDays(days)
+                .format(ISO_LOCAL_DATE);
     }
 }
