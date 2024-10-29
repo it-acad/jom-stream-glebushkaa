7d681c624b9436cad1b4d574b8964c099517225a
gle.bushkaa <gleb.mokryy@gmail.com>
2024-10-29 09:38:38 +0200

    finished

--- src/main/java/jom/com/softserve/s6/task3/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task3/MyUtils.java
@@ -1,11 +1,20 @@
 package jom.com.softserve.s6.task3;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class MyUtils {
 
-    //Write your code here
-
+    static void writeFile(String filename, String text) {
+        try (FileWriter outputStream = new FileWriter(filename)) {
+            var builder = new StringBuilder();
+            for (char ch : text.toCharArray()) {
+                String binaryString = String.format("%7s", Integer.toBinaryString(ch)).replace(' ', '0');
+                builder.append(binaryString);
+            }
+            outputStream.write(builder.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
