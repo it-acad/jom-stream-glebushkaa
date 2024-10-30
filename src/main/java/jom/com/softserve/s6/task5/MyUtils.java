71a43163ef11bf5ae77d33a1a97a3d832c1d1e07
gle.bushkaa <gleb.mokryy@gmail.com>
2024-10-30 09:26:22 +0200

    finished

--- src/main/java/jom/com/softserve/s6/task5/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task5/MyUtils.java
@@ -1,12 +1,25 @@
 package jom.com.softserve.s6.task5;
 
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MyUtils {
 
-    //Write your code here
-
+    Stream<String> nameList(Map<String, Stream<String>> map) {
+        if (map == null) throw new NullPointerException();
+        return map.values().stream()
+                .flatMap(persons -> {
+                    if (persons == null) return Stream.of();
+                    return persons.toList().stream();
+                })
+                .filter(person -> Objects.nonNull(person) && !person.isBlank())
+                .map(name -> name.toLowerCase().replaceAll("\\s+", ""))
+                .collect(Collectors.toCollection(LinkedHashSet::new))
+                .stream()
+                .map(name -> Character.toUpperCase(name.charAt(0)) + name.substring(1))
+                .sorted();
+    }
 }
